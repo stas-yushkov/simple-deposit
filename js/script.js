8dae7a216693b8cc9dd375e15a9fc29fb5dc169b
stas-yushkov <Stas.YUshkov@gmail.com>
2021-05-01 20:00:36 +0300

    Upd alert, markup

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 
 const days = prompt('Скільки днів плануєш збирати гроші? ');
+let message = 'Не плануєш збирати? Шкода. \nБо за 365 днів міг би назбирати суму: 66795.';
 const total = function (inputDays) {
   const days = Number(inputDays);
   let sum =0;
-  let message = ''
+  
   if (!days) {
-    return `Не плануєш збирати? Шкода. \nБо за 365 днів міг би назбирати суму: 66795.`
+    return `<p>Не плануєш збирати? Шкода. \nБо за 365 днів міг би назбирати суму: 66795.</p>` ;
   }
   for (let i = 1; i <= days; i+=1) {
     sum += i;
   }
-  return  `За ${days} дня/днів/день назбираєш сумму: ${sum}, \nА за 365 днів: 66795.` ;
+  message = `За ${days} дня/днів/день назбираєш сумму: ${sum}, \nА за 365 днів: 66795.`
+  return `<p>За ${days} дня/днів/день назбираєш сумму: ${sum}, \nА за 365 днів: 66795.</p>` ;
 }
 
 
-alert( total(days) );
+//alert( total(days) );
+
+const markup = total(days);
+alert(message);
+
+
+document.body.insertAdjacentHTML('beforeend', markup);
