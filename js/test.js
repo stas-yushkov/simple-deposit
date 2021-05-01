c78454b742cf9f0707ed0b65f879706cd8006bb3
stas-yushkov <Stas.YUshkov@gmail.com>
2021-05-01 17:01:00 +0300

    Ukrainian, color

--- js/test.js
+++ js/test.js
@@ -1,11 +1,11 @@
-//const numbers = [-11, 11, 0, 2, 4]
-const days = prompt('days');
+
+const days = prompt('Скільки днів плануєш збирати гроші? ');
 const total = function (days) {
   let sum =0;
   for (let i = 1; i <= days; i+=1) {
     sum += i;
   }
-  return `Days: ${days} sum: ${sum}, \nDays: 365 sum:  66795`;
+  return `За ${days} дня/днів/день назбираєш ${sum} грошей, \nА за: 365 днів -   66795.`;
 }
 
 
