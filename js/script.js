4c33af72b84afd49b2c84d5a55152c2c1a8318b8
stas-yushkov <Stas.YUshkov@gmail.com>
2021-05-01 19:20:31 +0300

    Upd cancelation message

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 
 const days = prompt('Скільки днів плануєш збирати гроші? ');
-const total = function (days) {
+const total = function (inputDays) {
+  const days = Number(inputDays);
   let sum =0;
+  let message = ''
+  if (!days) {
+    return `Не плануєш збирати? Шкода. \nБо за 365 днів набирав би суму: 66795.`
+  }
   for (let i = 1; i <= days; i+=1) {
     sum += i;
   }
-  return `За ${days} дня/днів/день назбираєш ${sum} грошей, \nА за: 365 днів -   66795.`;
+  return  `За ${days} дня/днів/день назбираєш сумму: ${sum}, \nА за 365 днів: 66795.` ;
 }
 
 
